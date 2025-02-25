c63bfa142bf7067dba6753962bf2663240207edc
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-24 20:21:03 -0400

    feat: Add campaigns management

--- main.py
+++ main.py
@@ -15,44 +15,47 @@ db = os.getenv('db')
 port = os.getenv('port')
 
 db = mysql.connector.connect(host=host, user=user, passwd=passwd, db=db, port=port)
-
 cursor = db.cursor()
 
-query = """
-    SELECT vc.dialable_leads 
-    FROM vicidial_campaign_stats vc
-    WHERE vc.campaign_id = 'C1'
-"""
-cursor.execute(query)
-rows = cursor.fetchall()
+campaigns = ['A1', 'A2', 'A4']
 
-dialable_leads = rows[0][0]
+for campaign in campaigns:
 
-if dialable_leads < 100:
-    Log("/var/log/reseteo-listas-vicidial/log", "Menos de 100 de registros, reseteando C1 (" + str(dialable_leads) + "): ")
-
-    # Update resets_today
-    query = """
-        UPDATE vicidial_lists S
-        SET resets_today=(resets_today + 1)
-        WHERE
-            S.campaign_id='C1'
-            AND S.active = 'Y'
-    """
-
-    # Update called_since_last_reset
-    cursor.execute(query)
-    query = """
-        UPDATE vicidial_list L 
-        JOIN vicidial_lists S on S.list_id=L.list_id
-        SET called_since_last_reset = 'N'
-        WHERE
-            S.campaign_id='C1'
-            AND S.active = 'Y'
-            AND called_since_last_reset = 'Y'
+    query = f"""
+        SELECT vc.dialable_leads 
+        FROM vicidial_campaign_stats vc
+        WHERE vc.campaign_id = '{campaign}'
     """
     cursor.execute(query)
-else:
-    now = datetime.datetime.now()
-    datetime_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    print("[" + datetime_str + "] " + str(dialable_leads) + " registros disponibles")
+    rows = cursor.fetchall()
+
+    dialable_leads = rows[0][0]
+
+    if dialable_leads < 100:
+        Log("/var/log/reseteo-listas-vicidial/log", f"Menos de 100 de registros, reseteando {campaign} (" + str(dialable_leads) + "): ")
+
+        # Update resets_today
+        query = f"""
+            UPDATE vicidial_lists S
+            SET resets_today=(resets_today + 1)
+            WHERE
+                S.campaign_id='{campaign}'
+                AND S.active = 'Y'
+        """
+
+        # Update called_since_last_reset
+        cursor.execute(query)
+        query = f"""
+            UPDATE vicidial_list L 
+            JOIN vicidial_lists S on S.list_id=L.list_id
+            SET called_since_last_reset = 'N'
+            WHERE
+                S.campaign_id='{campaign}'
+                AND S.active = 'Y'
+                AND called_since_last_reset = 'Y'
+        """
+        cursor.execute(query)
+    else:
+        now = datetime.datetime.now()
+        datetime_str = now.strftime("%Y-%m-%d %H:%M:%S")
+        print("[" + datetime_str + "] " + str(dialable_leads) + " registros disponibles")
